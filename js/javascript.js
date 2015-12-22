25bb054f902a549293286bc107c783166080064a
Alex Beaulieu <alex.m.beaulieu@gmail.com>
2015-12-21 23:32:08 -0800

    Added Choose color option, boostrap styling

--- js/javascript.js
+++ js/javascript.js
@@ -1,19 +1,29 @@
+
+
 var answer = prompt("How big would you like the drawing space to be? Choose up to 50");
 var yourNumber = parseInt(answer);
+
+alert('To start drawing -- select one of the coloring options!')
+ 
+
  
 $(document).ready(function() {
 	generateGrid(yourNumber);
 	graphSize();
-	mouseOverDefault();
 	clearGraph();
+	colored();
+	getRandomColor();
+	standard();
+	choose();
 	});
+	
 
 	
 function clearGraph(){
-	$('.clear').click(function(){
-		$('.colored').removeClass().addClass('grid');
+	$('#clear').click(function(){
 		$('.grid').css("background", "white");
 	});
+	mouseOverDefault();
 };
 
 
@@ -31,9 +41,18 @@ function graphSize(){
 
 function mouseOverDefault(){
 	$(".grid").mouseover(function(){
-		$(this).removeClass().addClass("colored"); 
+		$(this).css("background", 'black'); 
 	});
 };
+
+
+function standard (){
+$('#black').click(function(){
+    mouseOverDefault();
+});
+
+};
+
 
 
 
@@ -42,12 +61,32 @@ function getRandomColor() {
 var letters = '0123456789ABCDEF'.split(''); 
 var color = '#'; 
 for (var i =0; i < 6; i++){
-	color += letters[Math.floor(Math.random()*16)];
-	}
-	$('.colored').mouseover(function(){
-		$(this).css("background", color);
-	}); 
+	color += letters[Math.floor(Math.random()*16)]
+	}; 
+	return color; 
+};
+
+function colored(){
+$('#multicolor').click(function(){
+var randomColor = getRandomColor();
+ $('.grid').mouseover(function(){
+   $(this).css("background", randomColor);
+ });
+});
 }; 
+
+function choose(){
+$('#choose').click(function(){
+alert('Remember to choose a color the internet can understand. \n Examples include: black, blue, yellow, purple, grey, etc.');
+var yourColor = prompt('Choose a color!'); 
+ $('.grid').mouseover(function(){
+   $(this).css("background", yourColor);
+ });
+});
+};
+
+
+
 
 
  
